cc9b23144c1a03186d08b3b335a11fdad62645b8
richieadler <>
2022-10-06 03:15:38 +0000

    doccoll: rename inner object to __handle to be consistent
    FossilOrigin-Name: 8a0c8d450605c5550cef4130ef455ff5492cb669dd670540c9b68c0731fb142b

--- src/noteslib/doccoll.py
+++ src/noteslib/doccoll.py
@@ -4,26 +4,26 @@ class DocumentCollection:
 
     def __init__(self, nc):
         """Initialize with the passed collection"""
-        self.__coll = nc
+        self.__handle = nc
 
     def __len__(self):
-        return self.__coll.Count
+        return self.__handle.Count
 
     def __getitem__(self, item):
         if not isinstance(item, int):
             raise IndexError(f"{self.__class__.__name__} indices must be integers")
-        ix = (item % self.__len__()) + 1
-        return self.__coll.GetNthDocument(ix)
+        ix = (item % self.__handle.Count) + 1
+        return self.__handle.GetNthDocument(ix)
 
     def __iter__(self):
-        coll = self.__coll
+        coll = self.__handle
         doc = coll.GetFirstDocument()
         while doc:
             yield doc
             doc = coll.GetNextDocument(doc)
 
     def __reversed__(self):
-        coll = self.__coll
+        coll = self.__handle
         doc = coll.GetLastDocument()
         while doc:
             yield doc
@@ -31,4 +31,4 @@ class DocumentCollection:
 
     def __getattr__(self, name):
         """Delegate to the Notes object to support all properties and methods."""
-        return getattr(self.__coll, name)
+        return getattr(self.__handle, name)
